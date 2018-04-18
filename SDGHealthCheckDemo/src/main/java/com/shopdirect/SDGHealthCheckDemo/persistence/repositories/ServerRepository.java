3663ff78b6034f4b2ca9b8e5ac4fd4e4e624b377
abhpaul2 <abhijpau@in.ibm.com>
2018-04-19 00:11:03 +0530

    space2.

--- SDGHealthCheckDemo/src/main/java/com/shopdirect/SDGHealthCheckDemo/persistence/repositories/ServerRepository.java
+++ SDGHealthCheckDemo/src/main/java/com/shopdirect/SDGHealthCheckDemo/persistence/repositories/ServerRepository.java
@@ -4,5 +4,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.shopdirect.SDGHealthCheckDemo.persistence.models.Server;
 
 public abstract interface ServerRepository extends JpaRepository<Server, Long> {
-  
+ 
 }
